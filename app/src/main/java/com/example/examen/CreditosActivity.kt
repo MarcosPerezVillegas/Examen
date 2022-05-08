f04d61c604d55a93c47b3ce53be781db63a9475a
Marcos Antonio Perez Villegas <marcos.pvillegas@alumnos.udg.mx>
2022-05-07 20:07:34 -0500

    Boton regresar

--- app/src/main/java/com/example/examen/CreditosActivity.kt
+++ app/src/main/java/com/example/examen/CreditosActivity.kt
@@ -1,11 +1,20 @@
 package com.example.examen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class CreditosActivity : AppCompatActivity() {
+    lateinit var btnRegreso : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_creditos)
+        btnRegreso = findViewById(R.id.btnRegresar)
+
+        btnRegreso.setOnClickListener {
+            val intentMain= Intent(this,MainActivity::class.java)
+            startActivity(intentMain)
+        }
     }
 }
